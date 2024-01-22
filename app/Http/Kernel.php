9f9c5eb4c1c5fc50d4bd99383366f68e536f706e
grobuonis12 <dzikunskasugnius@gmail.com>
2024-01-22 21:07:20 +0200

    cors issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,10 +7,9 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        // ... other middleware
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
